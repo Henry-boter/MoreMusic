dfda7e8e500039c74358e99d4ad9461de3ddd0ed
Henry-boter <1971785019@qq.com>
2018-05-02 23:10:12 +0800

    tabswiper样式

--- config/index.js
+++ config/index.js
@@ -14,7 +14,8 @@ module.exports = {
 
     // Various Dev Server settings
     // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '192.168.2.112', // can be overwritten by process.env.HOST
+    // host: '192.168.2.112', // can be overwritten by process.env.HOST
+    host: '192.168.3.12', // can be overwritten by process.env.HOST
     port: 8040, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
